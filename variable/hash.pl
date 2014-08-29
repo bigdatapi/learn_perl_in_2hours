df7157401fe3700d5d706a06f7eb6ba078443e49
harryczhang <bigdatapi@qq.com>
2014-08-29 18:26:03 +0800

    Format hash.pl by calling perltidy

--- variable/hash.pl
+++ variable/hash.pl
@@ -1,9 +1,9 @@
-my %hash=(one=>1, two=>2);
-my $hash_ref = {three=>3,four=>4};
+my %hash = ( one => 1, two => 2 );
+my $hash_ref = { three => 3, four => 4 };
 my @hash_arr = %hash;
 print "@hash_arr\n";
 
-if (exists $hash{three}) {
+if ( exists $hash{three} ) {
     print "$hash{three}\n";
 }
 else {
@@ -14,6 +14,6 @@ foreach my $entry (%hash) {
     print "$entry\n";
 }
 
-foreach my $k (keys %hash) {
+foreach my $k ( keys %hash ) {
     print "$k=>$hash{$k}\n";
 }

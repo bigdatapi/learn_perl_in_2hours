ba20d8a36ecc6b964fe1ffad434bbbfdfe300d02
harryczhang <bigdatapi@qq.com>
2014-08-30 00:05:01 +0800

    Adjust topic position

--- quiz/val_err.pl
+++ quiz/val_err.pl
@@ -5,19 +5,9 @@
 use strict;
 use warnings;
 
-my $files = [ STDOUT, STDERR ];
-open my $fp, ">/tmp/xx.out";
-my $files = [ *STDOUT, *STDERR, $fp ];
-
 my $hash_ref = { one => 1, two => 2 };
 my $hash_ref = { 1 => one, 2 => two};
 print "$hash_ref->{one}\n";
-
-print $files[1] "stuff\n";
-print @$files[1] "stuff\n";
-print $files->[1] "stuff\n";
-print { $files->[1] } "stuff\n";
-close $fp;
 
 my $hash_ref2 = { k1 => { 1 ? 1 : undef } };
 my $hash_ref2 = { k1 => 1 ? 1 : undef };
@@ -36,6 +26,15 @@ print "last elem of \$arr_ref is " . $arr_ref[$#@$arr_ref] . "\n";
 print "last elem of \$arr_ref is " . $arr_ref[$#{$arr_ref}] . "\n";
 print "last elem of \$arr_ref is " . @$arr_ref[$#@$arr_ref] . "\n";
 print "last elem of \$arr_ref is " . @$arr_ref[$#{@$arr_ref}] . "\n";
+
+my $files = [ STDOUT, STDERR ];
+open my $fp, ">/tmp/xx.out";
+my $files = [ *STDOUT, *STDERR, $fp ];
+print $files[1] "stuff\n";
+print @$files[1] "stuff\n";
+print $files->[1] "stuff\n";
+print { $files->[1] } "stuff\n";
+close $fp;
 
 foreach my $item @arr {
 	print;
